c45868bf778f45f8d8359db36af70cce3c47c3e6
Beela <bla@senia.dk>
2022-04-25 14:04:03 +0200

    working on complex models

---
+++ producer/Kafka-Producer/Kafka-Producer/House.cs
@@ -0,0 +1,39 @@
+﻿using Avro;
+using Avro.Specific;
+
+namespace Kafka_Producer;
+
+internal class House : ISpecificRecord
+{
+    private static Schema _schema = Schema.Parse(File.ReadAllText("House.avsc"));
+    public string Location { get; set; }
+    public double WaterUsage { get; set; }
+    public double ElectricityUsage { get; set; }
+    public double HeatingUsage { get; set; }
+
+    public Schema Schema => _schema;
+
+    public object Get(int fieldPos)
+    {
+        switch (fieldPos)
+        {
+            case 0: return Location;
+            case 1: return WaterUsage;
+            case 2: return ElectricityUsage;
+            case 3: return HeatingUsage;
+            default: throw new AvroRuntimeException("Bad index " + fieldPos + " in Get()");
+        }
+    }
+
+    public void Put(int fieldPos, object fieldValue)
+    {
+        switch (fieldPos)
+        {
+            case 0: Location = (string)fieldValue; break;
+            case 1: WaterUsage = (double)fieldValue; break;
+            case 2: ElectricityUsage = (double)fieldValue;break;
+            case 3: HeatingUsage = (double)fieldValue;break;
+            default: throw new AvroRuntimeException("Bad index " + fieldPos + " in Put()");
+        }
+    }
+}
--- producer/Kafka-Producer/Kafka-Producer/Producer.cs
+++ producer/Kafka-Producer/Kafka-Producer/Producer.cs
@@ -1,6 +1,9 @@
 ﻿using Confluent.Kafka;
 using System;
 using Microsoft.Extensions.Configuration;
+using Avro.Generic;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
 
 namespace Kafka_Producer;
 
@@ -13,38 +16,55 @@ public class Producer
         //    Console.WriteLine("Please provide the configuration file path as a command line argument");
         //}
 
-        IConfiguration config = new ConfigurationBuilder()
-            .AddIniFile("Kafka-Producer.properties")
-            .Build();
+        //IConfiguration config = new ConfigurationBuilder()
+        //    .AddIniFile("Kafka-Producer.properties")
+        //    .Build();
 
-        const string topic = "water";
+        const string topic = "house";
 
-        string[] households = new[] { "A1", "A2", "A3", "A4", "A5", "A6a", "A6b", "A7", "B21", "B77", "C21", "D12", "H2" };
-        string[] usagePerHourInKilo = new[] { "10", "3", "4.1", "1.1", "0.2", "5"};
+        House[] houses = {
+            new House{Location = "A", ElectricityUsage = 14, HeatingUsage = 1.1, WaterUsage = 0.1},
+            new House{Location = "B", ElectricityUsage = 24, HeatingUsage = 1.2, WaterUsage = 1.1},
+            new House{Location = "C", ElectricityUsage = 34, HeatingUsage = 1.3, WaterUsage = 0.2},
+            new House{Location = "D", ElectricityUsage = 44, HeatingUsage = 1.4, WaterUsage = 3.1},
+            new House{Location = "E", ElectricityUsage = 54, HeatingUsage = 1.5, WaterUsage = 0.4},
+        };
 
-        using (var producer = new ProducerBuilder<string, string>(config.AsEnumerable()).Build())
+        using (var schemaRegistry = new CachedSchemaRegistryClient(new SchemaRegistryConfig { Url = "test"}))
+        using (var producer = new ProducerBuilder<Null, House>(new ProducerConfig { BootstrapServers = "localhost:9092" }).SetValueSerializer(new AvroSerializer<House>(schemaRegistry)).Build())
         {
+            //var houseSchema = (Avro.RecordSchema)Avro.Schema.Parse(File.ReadAllText("House.avsc"));
+
+
             var numProduced = 0;
             const int numMessage = 10;
             Random random = new Random();
-            for(int i = 0; i < numMessage; i++)
+            for (int i = 0; i < numMessage; i++)
             {
-                var house = households[random.Next(households.Count())];
-                var usage = usagePerHourInKilo[random.Next(usagePerHourInKilo.Count())];
-
-                producer.Produce(topic, new Message<string, string> { Key = house, Value = usage },
-                    (deliveryReport) =>
-                    {
-                        if(deliveryReport.Error != ErrorCode.NoError)
-                        {
-                            Console.WriteLine($"Failed to deliver message: {deliveryReport.Error}");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Produced event to topic {topic}: key = {house, -10} value = {usage}");
-                            numProduced += 1;
-                        }
-                    }
+                var house = houses[random.Next(houses.Count())];
+                //var record = new GenericRecord(houseSchema);
+                //record.Add(nameof(house.Location), house.Location);
+                //record.Add(nameof(house.WaterUsage), house.WaterUsage);
+                //record.Add(nameof(house.ElectricityUsage), house.ElectricityUsage);
+                //record.Add(nameof(house.HeatingUsage), house.HeatingUsage);
+                var message = new Message<Null, House> { Value = house };
+                producer.ProduceAsync(topic, message //,
+                    //(deliveryReport) =>
+                    //{
+                    //    if (deliveryReport.Error != ErrorCode.NoError)
+                    //    {
+                    //        Console.WriteLine($"Failed to deliver message: {deliveryReport.Error}");
+                    //    }
+                    //    else
+                    //    {
+                    //        Console.WriteLine($"Produced event to topic {topic}");
+                    //        numProduced += 1;
+                    //    }
+                    //}
+                ).ContinueWith(task => Console.WriteLine(
+                        task.IsFaulted
+                            ? $"error producing message: {task.Exception.Message}, {task.Exception.InnerException.InnerException.Message}"
+                            : $"produced to: {task.Result.TopicPartitionOffset}")
                 );
             }
 
