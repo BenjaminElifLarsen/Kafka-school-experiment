d77596bf75ab83046e191e82d04279d41f773e7c
Beela <bla@senia.dk>
2022-05-06 09:57:41 +0200

    comsumer data handling

---
+++ consumer/Kafka-Comsumer-Data-Handling/Kafka-Comsumer-Data-Handling/Models/Database/HouseDb.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+
+namespace Kafka_Comsumer_Data_Handling.Models.Database;
+
+internal class HouseDb
+{
+    public IList WaterUsasgePerDay { get; set; }
+    public IList ElectricityUsagePerDay { get; set; }
+    public IList HeatingUsagePerDay { get; set; }
+
+    public IList<(DateTime, double)> WaterSamples { get; set; }
+    public IList<(DateTime, double)> ElectricitySamples { get; set; }
+    public IList<(DateTime, double)> HeatingSamples { get; set; }
+}
---
+++ consumer/Kafka-Comsumer-Data-Handling/Kafka-Comsumer-Data-Handling/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+/* Data handling
+ * Avg use per house per day.
+ * peak use per house per day
+ * lowest use per house per day.
+ * Top user per day/week/month
+ * Lowest user per day/week/month
+ * Save to database.
+ */
