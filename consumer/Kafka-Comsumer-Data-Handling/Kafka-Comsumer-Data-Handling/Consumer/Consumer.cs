cff803b44c0a7897cb7d10c2a5e2afcfcfabcfb8
Beela <bla@senia.dk>
2022-05-20 08:11:31 +0200

    added some logic to Consumer.Consume() and added new ctr

--- consumer/Kafka-Comsumer-Data-Handling/Kafka-Comsumer-Data-Handling/Consumer/Consumer.cs
+++ consumer/Kafka-Comsumer-Data-Handling/Kafka-Comsumer-Data-Handling/Consumer/Consumer.cs
@@ -3,6 +3,8 @@ using Confluent.Kafka.SyncOverAsync;
 using Confluent.SchemaRegistry;
 using Confluent.SchemaRegistry.Serdes;
 using Kafka;
+using Kafka_Comsumer_Data_Handling.Logics;
+using Kafka_Comsumer_Data_Handling.Repositories;
 
 namespace Kafka_Comsumer_Data_Handling.Consumer;
 
@@ -16,13 +18,21 @@ namespace Kafka_Comsumer_Data_Handling.Consumer;
 
 internal class Consumer
 {
-    private readonly string bsServer = "172.16.250.13:9092";
-    private readonly string asrServer = "172.16.250.12:8081";
+    private readonly string _bsServer = "172.16.250.13:9092";
+    private readonly string _asrServer = "172.16.250.12:8081";
     private const string _topic = "house";
     private readonly ConsumerConfig _config;
-    private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+    private readonly CancellationTokenSource _cts = new();
+    private readonly HouseRepository _houseRepository; //move this and the other repo into a Unit of Work
+    private readonly OverviewRepository _overviewRepository;
 
-    public Consumer()
+    public Consumer(HouseRepository houseRepository, OverviewRepository overviewRepository) : this()
+    {
+        _houseRepository = houseRepository;
+        _overviewRepository = overviewRepository;
+    }
+
+    private Consumer()
     {
         Console.CancelKeyPress += (_, e) => {
             e.Cancel = true;
@@ -31,14 +41,15 @@ internal class Consumer
         _config = new()
         {
             GroupId = Guid.NewGuid().ToString(),
-            BootstrapServers = bsServer,
+            BootstrapServers = _bsServer,
             AutoOffsetReset = AutoOffsetReset.Latest
         };
     }
 
     public void Consume()
     {
-        using (var schemaRegistry = new CachedSchemaRegistryClient(new SchemaRegistryConfig { Url = asrServer }))
+        var houseLogic = new ConsumerLogic(_houseRepository, _overviewRepository);
+        using (var schemaRegistry = new CachedSchemaRegistryClient(new SchemaRegistryConfig { Url = _asrServer }))
             using (var consumer = new ConsumerBuilder<Null, House>(_config).SetValueDeserializer(new AvroDeserializer<House>(schemaRegistry).AsSyncOverAsync()).Build())
             {
                 consumer.Subscribe(_topic);
@@ -47,12 +58,17 @@ internal class Consumer
                     while (true)
                     {
                         var cr = consumer.Consume(_cts.Token);
-                        var house = cr.Value;
+                        var house = cr.Message.Value;
+                        houseLogic.Calculations(house);
                     }
                 }
                 catch (OperationCanceledException)
                 {
                     // Ctrl-C was pressed.
+                }
+                catch (Exception e)
+                {
+                    // Log the error.
                 }
                 finally
                 {
